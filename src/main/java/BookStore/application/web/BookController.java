fa50290507d05f85fddcf31318656cf880d7b21a
Jenny Berg <berg.jenny7@gmail.com>
2021-09-06 21:38:37 +0300

    Ensimmäinen versio

---
+++ src/main/java/BookStore/application/web/BookController.java
@@ -0,0 +1,16 @@
+package BookStore.application.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BookController {
+	
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	
+	
+
+}
